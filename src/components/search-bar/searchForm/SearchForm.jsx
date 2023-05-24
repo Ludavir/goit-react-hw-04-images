292eddccc1b52e7a3ec1b697fb51b353453a4b8b
ludavir <david_arq.violin@yahoo.com>
2023-05-23 22:21:59 -0400

    update

--- src/components/search-bar/searchForm/SearchForm.jsx
+++ src/components/search-bar/searchForm/SearchForm.jsx
@@ -1,43 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../../../styles/searchForm.css';
 import PropTypes from 'prop-types';
 
-class SearchForm extends Component {
-  state = {
-    search: '',
-  };
-  onSearchInput = e => {
-    const value = e.currentTarget.value;
-    this.setState({ search: value });
-  };
-  render() {
-    const { submitFn } = this.props;
-    const { search } = this.state;
-    return (
-      <form
-        className="searchForm"
-        onSubmit={e => {
-          e.preventDefault();
-          submitFn(search);
-        }}
-      >
-        <button type="submit" className="searchFormButton">
-          <span className="searchFormLabel">Search</span>
-        </button>
+const SearchForm = ({ submitFn }) => {
+  const [search, setSearch] = useState('');
 
-        <input
-          className="searchFormInput"
-          type="text"
-          value={this.state.search}
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-          onChange={this.onSearchInput}
-        />
-      </form>
-    );
-  }
-}
+  const onSearchInput = e => {
+    const value = e.currentTarget.value;
+    setSearch(value);
+  };
+  return (
+    <form
+    className="searchForm"
+    onSubmit={e => {
+      e.preventDefault();
+      submitFn(search);
+    }}
+  >
+    <button type="submit" className="searchFormButton">
+      <span className="searchFormLabel">Search</span>
+    </button>
+
+    <input
+      className="searchFormInput"
+      type="text"
+      value={search}
+      autoComplete="off"
+      autoFocus
+      placeholder="Search images and photos"
+      onChange={onSearchInput}
+    />
+  </form>
+  );
+};
+
 export default SearchForm;
 SearchForm.propTypes = {
   submitFn: PropTypes.func,
